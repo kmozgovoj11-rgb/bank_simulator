6f4e2eeeb48d581492432d2443eddbae03d1ecc2
konstantidtrapsh <konstantomafioznik@gmail.com>
2026-03-31 23:15:00 +0300

    add classes for sqlite

--- domain/model/DepositTransaction.java
+++ domain/model/DepositTransaction.java
@@ -48,5 +48,9 @@ public class DepositTransaction extends Transaction {
     public List<String> getInvolvedAccountNumbers() {
         return List.of(targetAccount.getNumber());
     }
+    
+    public Account getTargetAccount() {
+        return targetAccount;
+    }
 }
 
